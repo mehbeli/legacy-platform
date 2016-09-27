08822c70c4c50c5e94cf93534b4f39c4179bdb7c
Hakim Razalan <akimrr@gmail.com>
2016-09-27 23:14:37 +0800

    coming soon page

---
+++ resources/views/comingsoon.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Coming Soon</title>
+        <style media="screen">
+            html {
+                font-family: Lato;
+                background: #EEE;
+            }
+            .jejual {
+                max-width: 200px;
+                height: auto;
+            }
+            .center {
+                text-align: center;
+                margin-top: 10%;
+            }
+
+            .coming-soon {
+                margin-top: 20px;
+                font-size: 28px;
+                text-transform: uppercase;
+                font-weight: bold;
+                color: #686868;
+            }
+
+            .info {
+                font-size: 14px;
+                margin-top: 20px;
+                text-transform: uppercase;
+            }
+
+            /* .coming-text {
+                padding: 10px;
+                color: #FFF;
+                background-color: #686868;
+                border-radius: 5px;
+            } */
+            .info_mid {
+                font-size: 21px;
+            }
+
+            .simple {
+                padding: 10px;
+                background: #cecece;
+                color: #FFF;
+            }
+
+        </style>
+    </head>
+    <body>
+        <div class="center">
+            <img src="/images/jejuallogo.png" alt="" class="jejual" />
+            <div class="coming-soon">
+                <span class="coming-text">Coming Soon!</span>
+            </div>
+
+            <div class="info">
+                <div class="info_one">
+                    <span class="simple">Simple Business Management System</span>
+                </div>
+            </div>
+
+        </div>
+    </body>
+</html>
